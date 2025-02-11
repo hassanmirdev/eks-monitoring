d0d1ad3ce0f88c5fda8db69f28d02e2b3540da5a
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-11 18:21:02 +0530

    Update output.tf

--- infra/modules/eks/output.tf
+++ infra/modules/eks/output.tf
@@ -1,8 +1,10 @@
 # Output EKS Cluster and Fargate Profile details
 output "eks_cluster_name" {
   value = aws_eks_cluster.main.name
+  description = "eks cluster name"
 }
 
 output "eks_cluster_endpoint" {
   value = aws_eks_cluster.main.endpoint
+  description = "eks cluster endpoint"
 }
