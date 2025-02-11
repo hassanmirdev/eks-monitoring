99d69fcd644350d32c40a412914bf55fd264ae39
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-11 18:27:11 +0530

    Update outputs.tf

--- infra/environment/dev/outputs.tf
+++ infra/environment/dev/outputs.tf
@@ -1,16 +1,23 @@
 output "eks_cluster_name" {
   value = module.eks.eks_cluster_name
+  description ="eks cluster name"
 }
+
 
 output "eks_cluster_endpoint" {
   value = module.eks.eks_cluster_endpoint
+  description ="eks cluster endpoint"
 }
+
 
 
 output "patient_service_repo_uri" {
   value = module.ecr.patient_service_repo_uri
+  description ="patient service repo url"
 }
+
 
 output "appointment_service_repo_uri" {
   value = module.ecr.appointment_service_repo_uri
+ description ="appointment service repo url"
 }
