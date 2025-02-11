6b281869fb5d0025072c6b52ec4f333dcbc67790
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-11 18:18:35 +0530

    Update output.tf

--- infra/modules/vpc/output.tf
+++ infra/modules/vpc/output.tf
@@ -1,22 +1,27 @@
 
 output "vpc_id" {
   value = aws_vpc.main.id
+   description = "VPC ID id"
 }
 
 
 output "private_subnet_ids" {
   value = aws_subnet.private[*].id
+  description = "private subnets id"
 }
 
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
+  description = "public subnets id"
 }
 
 output "internet_gateway_id" {
   value = aws_internet_gateway.main.id
+  description = "Internet gateway id"
 }
 
 output "nat_gateway_id" {
   value = aws_nat_gateway.main.id
+  description = "Nat gateway id"
 }
 
